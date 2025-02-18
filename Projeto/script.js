e9b3576428f06a8292253b293a1afa9569811efc
gabriel-ortolani <gabrielortolanisenai@gmail.com>
2025-02-18 13:19:41 -0300

    projeto-suco

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,3 +1,54 @@
-const aperto = () =>{
-    window.alert("deu certo")
+// const aperto = () =>{
+//     window.alert("deu certo");
+//     console.log("deu certo");
+// }
+
+let lista = document.querySelectorAll('.item');
+let next = document.getElementById('depois');
+let prev = document.getElementById('antes');
+let contar = lista.length;
+let ativo = 0;
+
+// Eu fiz
+// next.onclick = () =>{
+//     let desativar = document.querySelector('.ativo')
+//     desativar.classList.remove('ativo')
+//     if(ativo == 2){
+//         ativo = ativo - 2
+//     }else{
+//         ativo = ativo + 1
+//     }
+//     lista[ativo].classList.add('ativo')
+// }
+// prev.onclick = () =>{
+//     let desativar = document.querySelector('.ativo')
+//     desativar.classList.remove('ativo')
+//     if(ativo == 0){
+//         ativo = ativo + 2
+//     }else{
+//         ativo = ativo - 1
+//     }
+//     lista[ativo].classList.add('ativo')
+// }
+
+// Raul fez
+next.onclick = () =>{
+    let desativar = document.querySelector('.ativo')
+    desativar.classList.remove('ativo')
+    if(ativo >= contar -1){
+        ativo=0;
+    }else{
+        ativo = ativo + 1
+    }
+    lista[ativo].classList.add('ativo')
+}
+prev.onclick = () =>{
+    let desativar = document.querySelector('.ativo')
+    desativar.classList.remove('ativo')
+    if(ativo <= 0){
+        ativo = ativo + 2
+    }else{
+        ativo = ativo - 1
+    }
+    lista[ativo].classList.add('ativo')
 }
